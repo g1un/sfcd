c2862e957062030b6a111624d0de8287f16d0608
I.Smal <serbov@pochta.ru>
2017-03-23 18:06:54 +0300

    adaptivity is ready

--- js/script.js
+++ js/script.js
@@ -13,12 +13,22 @@ $(document).ready(function() {
 		function getOptions($slider) {
 			var slide = '#' + $slider.getAttribute('id') + ' .js-slider-item';
 			var slidesToShow = $slider.getAttribute('data-slides');
+			var slidesToShowSm = $slider.getAttribute('data-slides-sm');
+			var dots = $slider.getAttribute('data-dots');
 
 			return {
 				slide: slide,
 				slidesToShow: slidesToShow ? slidesToShow : '1',
-				dots: true,
-				arrows: false
+				dots: dots ? dots : true,
+				arrows: false,
+				responsive: [
+					{
+						breakpoint: 1024,
+						settings: {
+							slidesToShow: slidesToShowSm ? slidesToShowSm : '1'
+						}
+					}
+				]
 			};
 		}
 
